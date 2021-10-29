57278529703596366fd91e6e2651e0d7b05c9b9b
fnyaung <fnyaung@gmail.com>
2021-10-28 18:41:06 -0700

    react basics

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <div className="expenses">
       <ExpenseItem
@@ -21,6 +21,6 @@ function Expenses(props) {
       />
     </div>
   );
-}
+};
 
 export default Expenses;
