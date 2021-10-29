62e9cb1535ed8ff66b907c21b82d3688caff33e0
fnyaung <fnyaung@gmail.com>
2021-10-28 17:28:34 -0700

    React basics

---
+++ react-complete-guide/src/components/Expenses.js
@@ -0,0 +1,26 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
